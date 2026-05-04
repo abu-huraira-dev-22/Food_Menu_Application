d961484a9e98967f8398bc11dfaa991d87e889a8
Abu Huraira Ansari <your-github-email@example.com>
2026-05-04 20:32:21 +0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ function App() {
   
 
   return (
-   <h1>Food Menu Application</h1>
+    <div>
+      <h1 id='heading'>Food Menu Application</h1>
+      <h3>Our Menu</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet quas fuga iusto dolorum natus reiciendis itaque quos voluptate veniam distinctio fugit impedit in ea dignissimos quasi ducimus quidem, ratione ex.</p>
+      <ul>
+        <li>
+          <img src="https://th.bing.com/th/id/OIP.bMaDg5fHsyqpstZo0S8sqgHaE7?w=274&h=183&c=7&r=0&o=7&pid=1.7&rm=3" alt="" />
+          <h4>Title</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, obcaecati.</p>
+          <p>0</p>
+        </li>
+      </ul>
+    </div>
   )
 }
 
